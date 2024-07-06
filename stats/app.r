f15c87e8dfba82ae0ab54a6bbf9f612a5ae36e56
Wojciech Przybyła <contact@wpww.pl>
2024-07-06 21:58:06 +0200

    📊 summaries added

--- stats/app.r
+++ stats/app.r
@@ -1,47 +1,102 @@
 library(shiny)
-library(dplyr)
-library(ggplot2)
+library(dplyr); library(ggplot2); library(lubridate); library(stringr); library(scales)
 library(RMySQL)
 
-# database connection
-# con <- dbConnect(MySQL(),
-#   host = Sys.getenv("DB_HOST"),
-#   port = as.numeric(Sys.getenv("DB_PORT")),
-#   dbname = Sys.getenv("DB_DATABASE"),
-#   user = Sys.getenv("DB_USERNAME"),
-#   password = Sys.getenv("DB_PASSWORD")
-# )
-
-ui <- fluidPage(
-  titlePanel("Rachunek sumienia"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins", "Number of bins:", 1, 50, 30)
-    ),
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-)
-
-server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    x <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    hist(
-      x,
-      breaks = bins,
-      col = "red",
-      border = "white",
-      xlab = "Waiting",
-      main = "bleble"
-    )
-  })
-}
-
 shinyApp(
-  ui,
-  server,
+  ui = fluidPage(
+    titlePanel("Statystyki"),
+    tabsetPanel(
+      tabPanel(
+        "Przepływy",
+        fluid = TRUE,
+        sidebarLayout(
+          sidebarPanel(
+            dateRangeInput(
+              "datesRange", "Okres:",
+              floor_date(Sys.Date() - years(1), "month"),
+              separator = "do"
+            )
+          ),
+          mainPanel(
+            plotOutput("breakdown"),
+            plotOutput("balance")
+          )
+        )
+      )
+    )
+  ),
+  server = function(input, output) {
+    # database connection
+    con <- dbConnect(MySQL(),
+      host = Sys.getenv("DB_HOST"),
+      port = as.numeric(Sys.getenv("DB_PORT")),
+      dbname = Sys.getenv("DB_DATABASE"),
+      user = Sys.getenv("DB_USERNAME"),
+      password = Sys.getenv("DB_PASSWORD")
+    )
+
+    # validate(need(
+    #   input$datesRange[2] >= input$datesRange[1],
+    #   "Data końcowa musi być większa niż początkowa"
+    # ))
+
+    theme_set(theme_minimal())
+
+    transactions_summary <- reactive({
+      dbGetQuery(con, "
+        SELECT date, amount
+        FROM transactions
+      ") %>%
+      mutate(
+        date = as_date(date),
+        month = paste(
+          str_pad(year(date), 4, "left", 0),
+          str_pad(month(date), 2, "left", 0),
+          sep = "-"
+        ),
+        is_income = as.factor(if_else(amount > 0, "Przychody", "Wydatki"))
+      ) %>%
+      filter(date %within% interval(
+        as_date(as.POSIXct(input$datesRange[1])),
+        as_date(as.POSIXct(input$datesRange[2]))
+      )) %>%
+      arrange(month, is_income)
+    })
+
+    output$breakdown <- renderPlot(
+      transactions_summary() %>%
+        mutate(amount = abs(amount)) %>%
+        group_by(month, is_income) %>%
+        summarise(amount = sum(amount)) %>%
+        ggplot(aes(x = month, y = amount, fill = is_income)) +
+          geom_col(position = "dodge") +
+          theme(legend.position = "top") +
+          scale_y_continuous(labels = label_number()) +
+          scale_fill_manual(
+            name = "Typ",
+            labels = c("Przychody", "Wydatki"),
+            values = c("chartreuse3", "brown2")
+          ) +
+          labs(
+            title = "Przychody vs wydatki",
+            x = "Miesiąc",
+            y = "Kwota [zł]"
+          )
+    )
+    output$balance <- renderPlot(
+      transactions_summary() %>%
+        group_by(month) %>%
+        summarise(amount = sum(amount)) %>%
+        ggplot(aes(x = month, y = amount)) +
+          geom_col() +
+          scale_y_continuous(labels = label_number()) +
+          labs(
+            title = "Bilans",
+            x = "Miesiąc",
+            y = "Kwota [zł]"
+          )
+    )
+  },
   options = list(
     host = "0.0.0.0",
     port = 8001

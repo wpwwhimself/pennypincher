7490379ba08c06fe1f9f0733b11eb0542eca6d95
Wojciech Przybyła <contact@wpww.pl>
2024-07-25 22:45:51 +0200

    🚧 continued work on summary plots

--- stats/app.r
+++ stats/app.r
@@ -1,25 +1,33 @@
 library(shiny)
-library(dplyr); library(ggplot2); library(lubridate); library(stringr); library(scales)
+library(tidyverse)
+library(scales)
 library(RMySQL)
+library(dotenv)
 
 shinyApp(
   ui = fluidPage(
     titlePanel("Statystyki"),
-    tabsetPanel(
-      tabPanel(
-        "Przepływy",
-        fluid = TRUE,
-        sidebarLayout(
-          sidebarPanel(
-            dateRangeInput(
-              "datesRange", "Okres:",
-              floor_date(Sys.Date() - years(1), "month"),
-              separator = "do"
-            )
-          ),
-          mainPanel(
+    sidebarLayout(
+      sidebarPanel(
+        dateRangeInput(
+          "datesRange", "Okres:",
+          floor_date(Sys.Date() - years(1), "month"),
+          separator = "do"
+        )
+      ),
+      mainPanel(
+        tabsetPanel(
+          tabPanel(
+            "Miesięcznie",
             plotOutput("breakdown"),
             plotOutput("balance")
+          ),
+          tabPanel(
+            "Majątek",
+            tableOutput("rollingSum")
+          ),
+          tabPanel(
+            "Kategorie"
           )
         )
       )
@@ -34,27 +42,32 @@ shinyApp(
       user = Sys.getenv("DB_USERNAME"),
       password = Sys.getenv("DB_PASSWORD")
     )
-
-    # validate(need(
-    #   input$datesRange[2] >= input$datesRange[1],
-    #   "Data końcowa musi być większa niż początkowa"
-    # ))
-
-    theme_set(theme_minimal())
-
-    transactions_summary <- reactive({
+    categories <- dbGetQuery(con, "SELECT * FROM categories")
+    accounts <- dbGetQuery(con, "SELECT * FROM accounts")
+    transactions <-
       dbGetQuery(con, "
-        SELECT date, amount
+        SELECT *
         FROM transactions
       ") %>%
+      mutate(date = as_date(date))
+
+    # global settings
+    theme_set(theme_minimal())
+
+    # data
+    transactions_summary <- reactive({
+      transactions %>%
+      select(date, amount) %>%
       mutate(
-        date = as_date(date),
         month = paste(
           str_pad(year(date), 4, "left", 0),
           str_pad(month(date), 2, "left", 0),
           sep = "-"
         ),
-        is_income = as.factor(if_else(amount > 0, "Przychody", "Wydatki"))
+        is_income = factor(
+          if_else(amount > 0, "Przychody", "Wydatki"),
+          c("Przychody", "Wydatki")
+        )
       ) %>%
       filter(date %within% interval(
         as_date(as.POSIXct(input$datesRange[1])),
@@ -62,7 +75,20 @@ shinyApp(
       )) %>%
       arrange(month, is_income)
     })
+    rolling_sum <- reactive({
+      transactions %>%
+      group_by(date, account_id) %>%
+      summarise(amount = sum(amount, na.rm = TRUE)) %>%
+      ungroup() %>%
+      pivot_wider(names_from = account_id, names_prefix = "account_", values_from = amount) %>%
+      mutate(across(starts_with("account"), ~ .x %>%
+        replace_na(0) %>%
+        cumsum()
+      ))
+      # todo odjąć przepływy z kategorii transferowych
+    })
 
+    # outputs
     output$breakdown <- renderPlot(
       transactions_summary() %>%
         mutate(amount = abs(amount)) %>%
@@ -96,6 +122,9 @@ shinyApp(
             y = "Kwota [zł]"
           )
     )
+    output$rollingSum <- renderTable(rolling_sum())
+
+    dbDisconnect(con)
   },
   options = list(
     host = "0.0.0.0",
